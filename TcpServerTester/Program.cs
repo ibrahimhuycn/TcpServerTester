f7cd422bf153d5417cc0bc83c1fc804964748cbd
ibrahimhuycn <ibrahim.hucyn@live.com>
2023-11-30 00:36:07 +0500

    Add project files.

---
+++ TcpServerTester/Program.cs
@@ -0,0 +1,50 @@
+﻿using SimpleTCP;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var filename = "C:\\Temp\\test.txt";
+        var port = 0;
+        var server = new SimpleTcpServer();
+
+        // Parse command line arguments for port
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i] == "--port" && i + 1 < args.Length)
+            {
+                if (int.TryParse(args[i + 1], out int parsedPort))
+                {
+                    port = parsedPort;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid port number.");
+                    return;
+                }
+                break;
+            }
+        }
+
+        if (port == 0)
+        {
+            Console.WriteLine("Port not specified.");
+            return;
+        }
+
+        server.Delimiter = 0x13;
+        server.DataReceived += (sender, msg) =>
+        {
+            var message = msg.MessageString;
+            Console.WriteLine("RX: " + message);
+            File.AppendAllText(filename, message);
+        };
+
+        server.Start(port);
+
+
+        Console.WriteLine("Press [Enter] key to exit.");
+        Console.ReadLine();
+        server.Stop();
+    }
+}
---
+++ TcpServerTester/Readme.md
@@ -0,0 +1,7 @@
+﻿# TCP Server Tester
+1. TCP server tester is a software that listens on a specified TCP port for all the IP addresses on the system.
+2. Writes all received data to a predetermined file on disk without any alterations. File: `C:\Temp\text.txt`
+
+### Usage
+1. Open powershell and run the following command assuming that port to listen is 1234.
+2. `.\TcpServerTester.exe --port 1234` 
